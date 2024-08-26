eb0fcdc5b502dd47c01d2dfb6e52ad4ff270f3ea
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-08-26 22:43:07 +0530

    Data Structure and Algorithms

--- Main.java
+++ Main.java
@@ -1,5 +1,82 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello World");
-    }
-}
+// import java.util.*;
+
+// public class Main {
+// public static void main(String[] args) {
+// // list interface k through linkedlist class ko call kr rhe hey \
+// List<Integer> al = new LinkedList<>();
+// al.add(12);
+// al.add(67);
+// al.add(134);
+// al.add(678);
+// System.out.println(al);
+// // using for each loop
+// for (int a : al) {
+// System.out.print(a + " ");
+// }
+// System.out.println();
+// // through iterator
+// Iterator itr = al.iterator();// non-generic (interface --->iterator )
+// while (itr.hasNext()) {
+// System.out.print(itr.next() + " ");
+// }
+// System.out.println();
+// Iterator<Integer> itr1 = al.iterator();
+// while (itr1.hasNext()) {
+// System.out.print(itr1.next() + " ");
+// }
+// System.out.println();
+// ListIterator ltr = al.listIterator();
+// while (ltr.hasNext()) {
+// System.out.print(ltr.next() + " ");
+// }
+// System.out.println();
+// ListIterator<Integer> ltr1 = al.listIterator();
+// while (ltr1.hasNext()) {
+// System.out.print(ltr1.next() + " ");
+// }
+// }
+// }
+
+// LinkedList :
+// all wrapper class used
+// --------------------------------------------------------------
+// LinkedList :
+// homogenous
+
+// -------------------------------------------------------------
+// IMPLEMENT THROUGH LIST INTERFACE :
+// LinkedList :
+// all wrapper class used
+// --------------------------------------------------------------
+// LinkedList :
+// homogenous
+// --------------------------------------------------------------
+// integer :
+// add elements :10
+
+// for lop
+// for each loop
+// list iterator
+// iterator
+
+// --------------------------------------------------------------
+// single element print
+// --------------------------------------------------------------
+// linkedlist :
+// even
+// odd
+// armstrong
+// palindrome
+// neon
+// reverse every number
+// every element factorial
+// every element table
+
+// every number digit count
+// every number digit product
+// every number even number find
+// every number odd product number find
+
+// prime
+
+// -----------------------------------------------------------
