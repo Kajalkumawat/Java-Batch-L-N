5d489aba61c7c84e498a3e241d818b90357c2fb1
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-08-29 17:13:18 +0530

    basic code of hashset

---
+++ BasicHash.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class BasicHash {
+    public static void main(String[] args) {
+        //hashset stored hetrogenous
+        HashSet hs = new HashSet<>();
+        hs.add("hello");
+        hs.add(67);
+        hs.add('u');
+        System.out.println(hs);
+    }
+}
