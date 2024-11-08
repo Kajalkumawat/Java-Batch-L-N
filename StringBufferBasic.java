83c360058a0ba5f297e1ea9f83b84293415bb9d9
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-08 10:06:59 +0530

    STRING BUFFER BASIC CODE IN JAVA

---
+++ StringBufferBasic.java
@@ -0,0 +1,7 @@
+public class StringBufferBasic {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("hello");
+        sb.append("world");
+        System.out.println(sb);
+    }
+}
