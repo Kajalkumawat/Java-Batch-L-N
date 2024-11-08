b9a1d9fb6df7614c3bf79a80a0957b190d814f50
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-08 09:52:45 +0530

    STRING IMMUTABLE CONCEPT

---
+++ StringBasic1.java
@@ -0,0 +1,9 @@
+public class StringBasic1 {
+    public static void main(String[] args) {
+        String s = new String("hello");
+        s.concat("world");
+        System.out.println(s);
+        s = s.concat("duniya");
+        System.out.println(s);
+    }
+}
