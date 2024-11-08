10e6a432d93a81c4a3f1b6362bdaba669fcc0c09
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-08 09:26:16 +0530

    HOW STRING IS IMMUTABLE IN JAVA

---
+++ StringBasic.java
@@ -0,0 +1,8 @@
+public class StringBasic {
+    public static void main(String[] args) {
+
+        String s = new String("world");
+        s.concat("hello");
+        System.out.println(s);
+    }
+}
