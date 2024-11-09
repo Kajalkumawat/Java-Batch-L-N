237c4fc37e78d1ae03a51efda7180c2aca7c8f17
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-09 09:17:12 +0530

    STRING BUILDER IS MUTABLE IN JAVA

---
+++ StringBuilderMutable.java
@@ -0,0 +1,7 @@
+public class StringBuilderMutable {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("hello ");
+        sb.append("builder");
+        System.out.println(sb);
+    }
+}
