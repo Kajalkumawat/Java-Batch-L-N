89aa0a509f17e64a6a728ecb3d8c574816cc8389
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-09 09:14:36 +0530

    STRING BUILDER BASIC CODE

---
+++ StringBuilderBasic.java
@@ -0,0 +1,6 @@
+public class StringBuilderBasic {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("hello world");
+        System.out.println(sb);
+    }
+}
