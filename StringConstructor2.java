9327baec3d29cc083dd5d4196283063f55a05d25
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-09 09:42:13 +0530

    STRING LITERAL CONSTRUCTOR IN JAVA

---
+++ StringConstructor2.java
@@ -0,0 +1,7 @@
+public class StringConstructor2 {
+    public static void main(String[] args) {
+        String s = "hello world";
+        String b = new String(s);
+        System.out.println(b);
+    }
+}
