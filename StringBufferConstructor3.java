b8fa4002c5d319fce101c24401b2d72af4a28ec3
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-09 10:04:51 +0530

    STRING CONSTRUCTOR OF STRING BUILDER IN JAVA

---
+++ StringBufferConstructor3.java
@@ -0,0 +1,7 @@
+public class StringBufferConstructor3 {
+    public static void main(String[] args) {
+        String s = "hello";
+        StringBuffer sb = new StringBuffer(s);
+        System.out.println(sb);
+    }
+}
