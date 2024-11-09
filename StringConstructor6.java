a5892d975824afc0b18e7bb273971eb0f67449ba
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-09 09:54:09 +0530

    CHAR ARRAY STRING CONSTRUCTOR IN JAVA

---
+++ StringConstructor6.java
@@ -0,0 +1,7 @@
+public class StringConstructor6 {
+    public static void main(String[] args) {
+        char a[] = { 'k', 'a', 'j', 'a', 'l' };
+        String s = new String(a);
+        System.out.println(s);
+    }
+}
