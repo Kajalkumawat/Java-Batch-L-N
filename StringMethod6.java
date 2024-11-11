8844f7868f49b56536c8a287cf0d739d7c694996
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-11 09:34:18 +0530

    REPLACE METHOD IN JAVA

---
+++ StringMethod6.java
@@ -0,0 +1,6 @@
+public class StringMethod6 {
+    public static void main(String[] args) {
+        String s = "java is a programming language";
+        System.out.println(s.replace('a', 'o'));
+    }
+}
