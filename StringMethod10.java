e93f7d06b3157b2cf141b1efd40c5b0dd9df5809
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-11 09:52:43 +0530

    INDEX OF METHOD IN JAVA

---
+++ StringMethod10.java
@@ -0,0 +1,6 @@
+public class StringMethod10 {
+    public static void main(String[] args) {
+        String name = "java is programming language";
+        System.out.println(name.indexOf('a'));
+    }
+}
