aed06eb34de05ea9470d91648044b15fab4af1c3
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-11 09:41:15 +0530

    TO LOWER CASE IN STRING

---
+++ StringMethod7.java
@@ -0,0 +1,10 @@
+public class StringMethod7 {
+    public static void main(String[] args) {
+        String s = "HELLO";
+        System.out.println(s.toLowerCase());
+        // java is a programming language (programming capital)
+        String name = "JAVA IS PROGRAMMING LANGAUGE";
+        String s1 = name.substring(10, 22);
+        System.out.println(s1.toLowerCase());
+    }
+}
