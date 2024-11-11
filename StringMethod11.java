69ccb16cf46e4c4a45be4615b552c724b80e7deb
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-11 09:53:56 +0530

    LAST INDEX OF STRING

---
+++ StringMethod11.java
@@ -0,0 +1,6 @@
+public class StringMethod11 {
+    public static void main(String[] args) {
+        String s = "java is programming language";
+        System.out.println(s.lastIndexOf('a'));
+    }
+}
