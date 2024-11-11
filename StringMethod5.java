91a76fef9c8ae5e53112fd617255f10f9673ded7
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-11 09:33:51 +0530

    SUBSTRING SECOND WAY IN JAVAS

---
+++ StringMethod5.java
@@ -0,0 +1,6 @@
+public class StringMethod5 {
+    public static void main(String[] args) {
+        String s = "java is a programming language";
+        System.out.println(s.substring(5, 9));
+    }
+}
