6df5cecd2dea0dbf456426da8cafbe3dfcd9d3a6
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-14 10:18:21 +0530

    PRIMTIVE DATA TYPE INTO WRAPPER CLASS

---
+++ Typecasting3.java
@@ -0,0 +1,9 @@
+public class Typecasting3 {
+    public static void main(String[] args) {
+        // pd convert wc
+        int a = 87;
+        Integer b = Integer.valueOf(a);
+        System.out.println(b);
+    }
+}
+ 
