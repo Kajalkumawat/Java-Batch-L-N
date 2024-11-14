c32d6aac21b2233f9a6ee9d20ea5d7b30018c259
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-14 10:17:51 +0530

    WRAPPER CLASS INTO PRIMITIVE DATA TYPE IN JAVA

---
+++ Typecasting2.java
@@ -0,0 +1,8 @@
+public class Typecasting2 {
+    public static void main(String[] args) {
+        // wc convert into pd
+        Integer i = new Integer(987);
+        int b = i;
+        System.out.println(b);
+    }
+}
