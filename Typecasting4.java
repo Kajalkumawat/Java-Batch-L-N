79a2b7b5028ab01922ebe95f096e430572b23fdb
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-15 09:24:22 +0530

    WRAPPER CLASS CONVERT INTO STRING

---
+++ Typecasting4.java
@@ -0,0 +1,7 @@
+public class Typecasting4 {
+    public static void main(String[] args) {
+        // wrapper class convert into string
+        Integer i = 89;
+        System.out.println(i.toString());
+    }
+}
